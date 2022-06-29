740e0114d34b13d5eaff8b8c19d9ee2406a7dfda
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-06-29 07:52:56 -0400

    commit tree dc9974716c60ea191c74611f4b4ef53e54db2961

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(x)
+    while x > 0
+        puts "#{x} SECOND(S)!"
+     x -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds_to_midnight)
+    while seconds_to_midnight > 0
+      puts "#{seconds_to_midnight} SECOND(S)!"
+      sleep(1)
+      seconds_to_midnight -= 1
+    end
+    "HAPPY NEW YEAR!"
+  end
